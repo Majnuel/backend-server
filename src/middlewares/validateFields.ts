b33e59a67d3ce735d5159ad16663617645327c31
Emmanuel Calle <emacalle@hotmail.com>
2022-08-30 16:25:13 -0300

    response modified

--- src/middlewares/validateFields.ts
+++ src/middlewares/validateFields.ts
@@ -5,9 +5,11 @@ const validateFields = (
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.redirect("/missing-fields");
+  const { username, password, name, address, age, phone } = req.body;
+  console.log(req.body);
+
+  if (!username || !password || !name || !address || !age || !phone) {
+    res.status(400).json({ msg: "missing fields" });
   }
   next();
 };
